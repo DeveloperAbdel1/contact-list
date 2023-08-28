f0a9c894d9220f8f6c77b64198e88aaf2dfa5e39
DeveloperAbdel <abdel.bakheit@gmail.com>
2023-08-28 09:45:13 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+import './App.css'
+import ContactList from './components/ContactList';
+import ContactRow from './components/ContactRow';
+export default function App() {
+  return (
+    <>
+      <ContactList />
+    </>
+  );
+}
---
+++ src/components/ContactRow.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export default function ContactRow({ contact }) {
+  return (
+    <tr>
+      <td>{contact.name}</td>
+      <td>{contact.email}</td>
+      <td>{contact.phone}</td>
+    </tr>
+  );
+}
